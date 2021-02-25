ae3bb2a7c05f5636fd9efcf7dfdabcc1de5b1a5e
coachnovak <daniel.novak@extremakonsulter.com>
2021-02-25 21:59:12 +0100

    fix: added reactions to room discussion

---
+++ app/ui/components/reaction.js
@@ -0,0 +1,39 @@
+import { base } from "/components/base.js";
+
+export class reaction extends base {
+	constructor (_properties = {}) {
+		super(Object.assign(_properties ? _properties : {}, {
+			isolated: false
+        }));
+
+		this
+			.property("sender", _properties.sender ? _properties.sender : null)
+			.property("reaction", _properties.reaction ? _properties.reaction : null)
+			.property("when", _properties.when ? _properties.when : null);
+
+		this.styles.push(`
+
+		`);
+
+	}
+
+	async connectedCallback () {
+		await super.connectedCallback();
+		const reactions = ["like", "heart", "grin-squint", "surprise", "sad-tear", "angry"];
+
+		this.append(`
+			<div class="reaction">
+				<div class="sender">${this.sender} reacted <span class="when" datetime="${this.when}"></span> with <i class="fad fa-${reactions[this.reaction]}"></i>.</div>
+			</div>
+		`);
+
+		setTimeout(() => this.visible = true, 100);
+
+		const when = this.use(".when", { query: true });
+		timeago.render(when);
+
+		this.on("disposing", () => timeago.cancel(when));
+	}
+}
+
+globalThis.customElements.define("app-reaction", reaction);
