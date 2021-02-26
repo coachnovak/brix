fbe3a7ffb9729fcf13e5d77012aeb2a43709758d
coachnovak <daniel.novak@extremakonsulter.com>
2021-02-26 12:57:56 +0100

    fix: changed content in doormat, added exclamation mark

--- app/ui/articles/doormat.js
+++ app/ui/articles/doormat.js
@@ -26,7 +26,7 @@ export default {
 
 			<div id="doormat-article">
 				<div id="doormat-article-celebration"><!-- --></div>
-				<h3>Digital celebration</h3>
+				<h3>Digital celebration!</h3>
 				<br />
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget feugiat ante, sed condimentum risus. In luctus congue pretium. Quisque vel nisi vulputate, consequat neque vel, tristique risus. Nullam facilisis nunc facilisis, pellentesque tellus in, pretium ipsum. Proin eleifend condimentum velit, at tempus dolor. Vivamus in purus egestas, cursus tortor et, aliquam orci. Fusce eu purus neque. Curabitur metus lectus, ornare id elementum sed, varius vitae dui. Cras pellentesque leo ut tortor consectetur, a imperdiet nibh posuere. Ut non lacus vel purus elementum sagittis ut porttitor turpis. Nam sem magna, hendrerit eget felis sit amet, vestibulum ultrices elit.<br />
 				<br />
