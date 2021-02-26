9e907af4e292d2d80adfa721c9449b686d9b2897
coachnovak <daniel.novak@extremakonsulter.com>
2021-02-26 10:19:02 +0100

    chore: updated content in doormat

--- app/ui/articles/doormat.js
+++ app/ui/articles/doormat.js
@@ -8,12 +8,12 @@ export default {
 	styles: `
 		#doormat-intro { border: 1px solid var(--paper-3); margin-bottom: 20px; border-radius: 3px; outline: 0; }
 
-		#doormat-article { margin-bottom: 60px; }
-		#doormat-article-what { float: left; clip-path: circle(); shape-outside: circle(45%); margin: 20px 20px 20px 0; padding: 10px; text-align: justify; position: relative; background: var(--paper-4); }
-		#doormat-article-what i { font-size: 64pt; }
+		#doormat-article { margin-bottom: 60px; text-align: justify; }
+		#doormat-article-celebration { float: left; clip-path: circle(); shape-outside: circle(45%); margin: 20px 20px 20px 0; text-align: justify; position: relative; background: white; }
+		#doormat-article-celebration { background-image: url(/assets/celebration.svg); background-size: contain; background-repeat: no-repeat; background-position: center center; padding: 60px; }
 
-		#doormat-article-how { float: right; clip-path: circle(); shape-outside: circle(45%); margin: 20px 0 20px 20px; padding: 10px; text-align: justify; position: relative; background: var(--paper-4); }
-		#doormat-article-how i { font-size: 64pt; }
+		#doormat-article-appreciation { float: right; clip-path: circle(); shape-outside: circle(45%); margin: 20px 0 20px 20px; padding: 10px; text-align: justify; position: relative; background: white; }
+		#doormat-article-appreciation { background-image: url(/assets/appreciation.svg); background-size: contain; background-repeat: no-repeat; background-position: center center; padding: 60px; }
 
 		#doormat-next { text-align: center; }
 	`,
@@ -25,10 +25,14 @@ export default {
 			</video>
 
 			<div id="doormat-article">
-				<div id="doormat-article-what"><i class="fad fa-users-class"></i></div>
+				<div id="doormat-article-celebration"><!-- --></div>
+				<h3>Digital celebration</h3>
+				<br />
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget feugiat ante, sed condimentum risus. In luctus congue pretium. Quisque vel nisi vulputate, consequat neque vel, tristique risus. Nullam facilisis nunc facilisis, pellentesque tellus in, pretium ipsum. Proin eleifend condimentum velit, at tempus dolor. Vivamus in purus egestas, cursus tortor et, aliquam orci. Fusce eu purus neque. Curabitur metus lectus, ornare id elementum sed, varius vitae dui. Cras pellentesque leo ut tortor consectetur, a imperdiet nibh posuere. Ut non lacus vel purus elementum sagittis ut porttitor turpis. Nam sem magna, hendrerit eget felis sit amet, vestibulum ultrices elit.<br />
 				<br />
-				<div id="doormat-article-how"><i class="fad fa-hammer"></i></div>
+				<div id="doormat-article-appreciation"><!-- --></div>
+				<h3>Appreciation builds teams</h3>
+				<br />
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget feugiat ante, sed condimentum risus. In luctus congue pretium. Quisque vel nisi vulputate, consequat neque vel, tristique risus. Nullam facilisis nunc facilisis, pellentesque tellus in, pretium ipsum. Proin eleifend condimentum velit, at tempus dolor. Vivamus in purus egestas, cursus tortor et, aliquam orci. Fusce eu purus neque. Curabitur metus lectus, ornare id elementum sed, varius vitae dui. Cras pellentesque leo ut tortor consectetur, a imperdiet nibh posuere. Ut non lacus vel purus elementum sagittis ut porttitor turpis. Nam sem magna, hendrerit eget felis sit amet, vestibulum ultrices elit.
 			</div>
 
