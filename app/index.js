da27cc209e0f3fb1780e418a4eda39bb82b179d8
coachnovak <daniel.novak@extremakonsulter.com>
2021-03-02 14:31:05 +0100

    fix: disabling debugging of a register

--- app/index.js
+++ app/index.js
@@ -26,8 +26,7 @@ const boot = async () => {
 	app.register(await import("./middleware/emailing.js"));
 
 	// Register api routes.
-	const appx = await import("./api/app.js");
-	app.register(appx, { prefix: "api/app" });
+	app.register(await import("./api/app.js"), { prefix: "api/app" });
 	app.register(await import("./api/security.js"), { prefix: "api/security" });
 	app.register(await import("./api/participant.js"), { prefix: "api/participant" });
 	app.register(await import("./api/participants.js"), { prefix: "api/participants" });
