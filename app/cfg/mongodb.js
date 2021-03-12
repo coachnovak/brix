39112ce28ee3e628fa299fb81540ee8851f40141
coachnovak <daniel.novak@extremakonsulter.com>
2021-03-12 09:19:37 +0100

    fix: added mongodb connectivity testing

--- app/cfg/mongodb.js
+++ app/cfg/mongodb.js
@@ -1,3 +1,17 @@
+import MongoDB from "mongodb";
+
+(async () => {
+	try {
+		const client = await MongoDB.MongoClient.connect(process.env.mongourl)
+		console.info("Connected to mongodb.");
+		client.close();
+
+	} catch {
+		console.log("Failed to connect to mongodb.");
+
+	}
+})();
+
 export const mongodb = {
 	url: process.env.mongourl,
 	forceClose: true
