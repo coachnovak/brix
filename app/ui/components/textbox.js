758bfade2047346296a22ce74818762db6ea5e43
coachnovak <daniel.novak@extremakonsulter.com>
2021-03-15 08:59:20 +0100

    fix: added autocomplete support to textbox

--- app/ui/components/textbox.js
+++ app/ui/components/textbox.js
@@ -9,7 +9,8 @@ export class textbox extends base {
 		this
 			.property("placeholder", _properties.placeholder ? _properties.placeholder : null)
 			.property("type", _properties.type ? _properties.type : null)
-			.property("center", _properties.center ? _properties.center : null);
+			.property("center", _properties.center ? _properties.center : null)
+			.property("autocomplete", _properties.autocomplete ? _properties.autocomplete : null);
 
         this.styles.push(`
 			#input { display: inline-block; background: var(--paper-2); border: 0px; color: var(--pen-1); padding: 15px; border-radius: 3px; width: 100%; }
@@ -22,8 +23,9 @@ export class textbox extends base {
 	async connectedCallback () {
 		await super.connectedCallback();
 
-		this.append(`<input type="${this.type}" id="input" placeholder="${this.placeholder}" />`);
+		this.append(`<input type="${this.type}" id="input" autocomplete="${this.autocomplete}" placeholder="${this.placeholder}" />`);
 		this.on("placeholder updated", _event => { this.use("input").setAttribute("placeholder", _event.detail); });
+		this.on("autocomplete updated", _event => { this.use("input").setAttribute("autocomplete", _event.detail); });
 
 		this.use("input").addEventListener("keydown", _event => { if (_event.key ==="Enter") this.emit("activated") });
 		this.use("input").addEventListener("keydown", _event => this.emit("changed"));
