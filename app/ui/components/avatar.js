d4041b4666242b6f928d46590785ce98d125b3d2
coachnovak <daniel.novak@extremakonsulter.com>
2021-03-31 22:24:55 +0200

    fix: added custom user presentation

--- app/ui/components/avatar.js
+++ app/ui/components/avatar.js
@@ -4,14 +4,16 @@ export class avatar extends component {
 	constructor (_properties = {}) {
 		super({ ..._properties });
 
-		const { size, clickable } = _properties;
+		const { size, clickable, user } = _properties;
 		this.property({ name: "size", value: size, options: { default: "m", isattribute: true } })
-			.property({ name: "clickable", value: clickable, options: { default: "false", isattribute: true } });
+			.property({ name: "clickable", value: clickable, options: { default: "false", isattribute: true } })
+			.property({ name: "user", value: user, options: { default: undefined } });
     }
 
 	connectedCallback ({ style, markup } = {}) {
 		super.connectedCallback({
 			style: component.template`
+				:host([size=xxs]) { --size: 15px; }
 				:host([size=s]) { --size: 40px; }
 				:host([size=m]) { --size: 90px; }
 				:host([size=l]) { --size: 150px; }
@@ -51,7 +53,7 @@ export class avatar extends component {
 	}
 
 	async refresh () {
-		await globalThis.fetcher(`/api/my/avatar/`, {
+		await globalThis.fetcher(`/api/${this.user ? "user" : "my"}/avatar/${this.user ? this.user._id : ""}`, {
 			method: "get"
 		}, {
 			200: async _response => {
@@ -71,8 +73,8 @@ export class avatar extends component {
 	}
 
 	async generate () {
-		const firstName = globalThis.session.identity.firstName.substring(0, 1);
-		const lastName = globalThis.session.identity.lastName.substring(0, 1);
+		const firstName = (this.user ? this.user.firstName : globalThis.session.identity.firstName).substring(0, 1);
+		const lastName = (this.user ? this.user.lastName : globalThis.session.identity.lastName).substring(0, 1);
 		const text = `${firstName.toUpperCase()} ${lastName.toUpperCase()}`;
 
 		const background = getComputedStyle(document.documentElement).getPropertyValue("--paper-2");
@@ -94,7 +96,7 @@ export class avatar extends component {
 		context.fillStyle = foreground;
 		context.textAlign = "center";
 		context.textBaseline = "middle";
-		context.fillText(text, (canvas.width / 2) - 1, (canvas.height / 2) + 2);
+		context.fillText(text, canvas.width / 2, (canvas.height / 2) + 1);
 
 		return canvas.toDataURL("image/png");
 	}
